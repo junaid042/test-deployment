fb4c706570536488dd6a737e8d37b2e67d961d50
Aamir Husnain <134502135+junaid042@users.noreply.github.com>
2024-02-23 23:26:15 +0500

    first commit

---
+++ src/taskpane/components/App.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import ReportPishing from "./ReportPishing";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Question/> */}
+      <ReportPishing />
+    </div>
+  );
+};
+
+export default App;
